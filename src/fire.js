98ba71926bfbf618ca4a325934c20649766ec02d
egoheru <egoherokugit@gmail.com>
2020-09-12 21:05:36 -0700

    updated

--- src/fire.js
+++ src/fire.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
- var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyCsYYNL8EmEHk6-HTreP5prEs-RQ9K1etE",
+  authDomain: "login-react-2a306.firebaseapp.com",
+  databaseURL: "https://login-react-2a306.firebaseio.com",
+  projectId: "login-react-2a306",
+  storageBucket: "login-react-2a306.appspot.com",
+  messagingSenderId: "1064741441742",
+  appId: "1:1064741441742:web:cddf7b6b42ec547196809f"
+};
 
 const fire =  firebase.initializeApp(firebaseConfig);
 
